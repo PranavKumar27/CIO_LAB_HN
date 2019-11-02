7e36ee6ea0afea14352c9e2b89cadf0d6b26b75f
PranavKumar27 <pranav.jenus@gmail.com>
2019-11-02 14:01:53 +0530

    First working commit

---
+++ workspace_Java/CIO_LAB/test.bat
@@ -0,0 +1,9 @@
+set HOST_IP=%1
+set FILE_NAME=%2
+
+
+scp -o "StrictHostKeyChecking no" C:\temp\%FILE_NAME% root@%HOST_IP%:/tmp/%FILE_NAME% >> C:\temp\%HOST_IP%\output1.txt 2>&1
+
+ssh -o "StrictHostKeyChecking no" root@%HOST_IP% HONEYWELL_APP_DIR=/usr/honeywell PATH=$PATH:/usr/honeywell/bin "upgrade.sh && ps && sleep 50s" >> C:\temp\%HOST_IP%\output2.txt 2>&1
+
+rem @RD /S /Q "C:\temp\%HOST_IP%"
